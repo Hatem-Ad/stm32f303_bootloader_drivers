2dc778f5aa715d6cc11b03494128d5751977b212
Hatem-Ad <adouani_hatem@yahoo.com>
2025-06-11 20:48:03 +0100

    Modif main

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,15 +1,9 @@
 #include "bootloader.h"
-#include "../drivers/gpio/gpio.h"
-#include "../drivers/uart/uart.h"
 
 int main(void) {
-    gpio_init();
-    uart_init();
     bootloader_init();
-
     while (1) {
         bootloader_run();
     }
-
     return 0;
 }
