6e02865efcd13cb876dac28078d3139e72d17e92
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-03 10:34:00 +0100

    Update main

--- Bootloader/Main.c
+++ Bootloader/Main.c
@@ -1,15 +1,8 @@
 #include "Bootloader.h"
 
 int main(void) {
-//init phase 
-    Bootloader_Init();
-    
-//Check the bootloader update
-    if (Bootloader_CheckForUpdate()) {
-        Bootloader_ReceiveFirmware();
-    }
-//jump to the apps 
-    Bootloader_JumpToApp();
+
+    Bootloader_run();
 
     while (1);
 }
