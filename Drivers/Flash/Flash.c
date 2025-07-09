60b4bcd63fb2cac74c21ab2c6fe052cf99c62bbc
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-10 00:13:17 +0100

    correct flash commit

--- Drivers/Flash/Flash.c
+++ Drivers/Flash/Flash.c
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 #include "Flash.h"
 #include "stm32f3xx.h" // needed for Flash erase
-=======
-#include "Flash.h"
-#include "stm32f3xx.h" // needed for Flash erase
-
->>>>>>> 4332afd3fa1789a8f5b2d385ebbaf93834a6b54f
