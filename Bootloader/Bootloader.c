1081b4c668f8aa0efdd6205b1efd13993669b186
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-23 23:53:08 +0100

    update

--- Bootloader/Bootloader.c
+++ Bootloader/Bootloader.c
@@ -31,9 +31,9 @@ void Bootloader_ReceiveFirmware(void) {
 
     //1. Erase the application aera before writing 
     FLASH_Unlock();
-    for(uint32_t currentAddress = APP_START_ADDRESS; currentAddress < (APP_START_ADDRESS + 0x2000); currentAddress += 0x800)
+    for(uint32_t pageAddr = APP_START_ADDRESS; pageAddr < (APP_START_ADDRESS + 0x2000); pageAddr += 0x800)
     {
-        Flash_Erase(currentAddress); //Erase one page (0x800 = 2KB for STM32f3)
+        Flash_Erase(pageAddr); //Erase one page (0x800 = 2KB for STM32f3)
     }
 
     //2. Received data chunks and write to flash
