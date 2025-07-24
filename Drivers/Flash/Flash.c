150c52b320af30ec2428649817d32249938b19e7
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2025-07-24 23:56:34 +0100

    Update Flash.c

--- Drivers/Flash/Flash.c
+++ Drivers/Flash/Flash.c
@@ -8,6 +8,7 @@ void Flash_Unlock(void)
     {
         //Write the two key values to unclock flash programming
 // Hard coded values, set by the st engineers 
+// Key1 : 0x45670123
         FLASH->KEYR = FLASH_KEY1;
         FLASH->KEYR = FLASH_KEY2;
     }
