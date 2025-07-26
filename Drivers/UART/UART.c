a8a7364d748411df1121b659ea95aef3448035f5
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-27 00:51:47 +0100

    update uart

--- Drivers/UART/UART.c
+++ Drivers/UART/UART.c
@@ -2,11 +2,29 @@
 #include "stdint.h"
 
 void UART_Init(void) {
-    // TODO: Init USART1 or USART2
+    
+    //Enble USART1 clock and GPIOA
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    //Configure PA9 (TX) and PA10(RX)
+    GPIOA->MODER &= ~((3U << (9 *2)) | (3U << (10 *2)));
+    GPIOA->MODER |= (2U << (9 * 2)) | (2U << (10 * 2)); //Alternate function
+
+    GPIOA->AFR[1] |= (7U << ((9 - 8) * 4)) | (7U << ((10 - 8) * 4)); // AF7
+
+    //Configure USART1 (9600 bauds, 8N1)
+    USART1->BRR = 8000000 / 9600;
+    USART1->CR1 = USART_CR_TE | USART_CR_RE | USART_CR_UE;
+
 }
 
-void uart_send_char(char c) {
-    // TODO: Send character via UART
+void UART_SendString(const char *str) {
+    
+    while(*str){
+        while (!(USART1->ISR & USART_ISR_TXE));
+        USART1->TDR = *str++;
+    }
 }
 
 // Read a chnik of at most len bytes
