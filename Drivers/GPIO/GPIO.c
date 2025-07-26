716abd4bc108c253f727a23c816c0c5cfc5de6b8
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-27 00:32:06 +0100

    update gpio and add userapp envirenement

--- Drivers/GPIO/GPIO.c
+++ Drivers/GPIO/GPIO.c
@@ -1,5 +1,19 @@
 #include "GPIO.h"
+#include <stdint.h>
 
 void GPIO_Init(void) {
-    // TODO: Configure LED or boot pin if needed
+    
+    //Enable the GPIO clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    //Configure PA0 as input(for a button)
+    GPIO->MODER &= ~(3U << (0 *2)); // 00: Input
+
+    //Configure an output LED : PA5
+    GPIOA->MODER |= (1U << (5 *2)); //01: Output
+    GPIOA->MODER &= ~(1U << (5 * 2 +1));
+}
+
+uint8_t GPIO_ReadPin(uint8_t pin_number){
+    return (GPIOA->IDR & (1 << pin_number)) ? 1 : 0;
 }
