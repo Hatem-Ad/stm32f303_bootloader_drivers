c3a8b676add6666e137db711eddbaa8679a73cf4
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-30 07:13:23 +0100

    update gpio.c

--- Drivers/GPIO/GPIO.c
+++ Drivers/GPIO/GPIO.c
@@ -21,3 +21,17 @@ void GPIO_Init(void) {
 uint8_t GPIO_ReadPin(uint8_t pin_number){
     return (GPIOA->IDR & (1 << pin_number)) ? 1 : 0;
 }
+
+void GPIO_WritePin(uint8_t pin_number, uint8_t state)
+{
+    if(state){
+        GPIOA->ODR |= (1U << pin_number); //Set pin hihg
+    } else {
+        GPIOA->ODR &= ~(1U << pin_number); // set pin low
+    }
+}
+
+//This function toggles the state of a given output pin on GPIOA.
+void GPIO_TogglePin(uint8_t pin_number){
+    GPIOA->ODR ^= (1U << pin_number);
+}
