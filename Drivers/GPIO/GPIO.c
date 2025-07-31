d351cc301108bdd2f8e7306ca657e3abf7b01f10
Hatem-Ad <adouani_hatem@yahoo.com>
2025-07-31 23:58:42 +0100

    update gpio write pin

--- Drivers/GPIO/GPIO.c
+++ Drivers/GPIO/GPIO.c
@@ -29,12 +29,12 @@ uint8_t GPIO_ReadPin(GPIO_TypeDef *port, uint8_t pin_number){
     return (port->IDR & (1 << pin_number)) ? 1 : 0;
 }
 
-void GPIO_WritePin(uint8_t pin_number, uint8_t state)
+void GPIO_WritePin(GPIO_TypeDef *port, uint8_t pin_number, uint8_t state)
 {
     if(state){
-        GPIOA->ODR |= (1U << pin_number); //Set pin hihg
+        port->ODR |= (1U << pin_number); //Set pin hihg
     } else {
-        GPIOA->ODR &= ~(1U << pin_number); // set pin low
+        port->ODR &= ~(1U << pin_number); // set pin low
     }
 }
 
