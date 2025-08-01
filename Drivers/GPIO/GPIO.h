de9562fc59951b358e24d835f1660c12d67270cc
Hatem-Ad <adouani_hatem@yahoo.com>
2025-08-01 18:26:40 +0100

    update update gpio.h

--- Drivers/GPIO/GPIO.h
+++ Drivers/GPIO/GPIO.h
@@ -2,8 +2,8 @@
 #define GPIO_H
 
 void GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode);
-uint8_t GPIO_ReadPin(uint8_t pin_number);
-void GPIO_WritePin(uint8_t pin_number, uint8_t state);
-void GPIO_TogglePin(uint8_t pin_number);
+uint8_t GPIO_ReadPin(GPIO_TypeDef *port, uint8_t pin_number);
+void GPIO_WritePin(GPIO_TypeDef *port, uint8_t pin_number, uint8_t state);
+void GPIO_TogglePin(GPIO_TypeDef *port, int8_t pin_number);
 
 #endif
