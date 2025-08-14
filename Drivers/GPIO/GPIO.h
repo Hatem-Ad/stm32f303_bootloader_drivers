53071a8c57ee6d6431216f93590711f78de2055e
Hatem-Ad <adouani_hatem@yahoo.com>
2025-08-14 02:19:55 +0100

    update gpio.h

--- Drivers/GPIO/GPIO.h
+++ Drivers/GPIO/GPIO.h
@@ -29,9 +29,9 @@ typedef enum {
 
 
 void GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode);
-uint8_t GPIO_ReadPin(GPIO_TypeDef *port, uint8_t pin_number);
-void GPIO_WritePin(GPIO_TypeDef *port, uint8_t pin_number, uint8_t state);
-void GPIO_TogglePin(GPIO_TypeDef *port, int8_t pin_number);
-
+void GPIO_Write(GPIO_TypeDef *port, uint8_t pin, uint8_t state);
+void GPIO_Toggle(GPIO_TypeDef *port, uint8_t pin);
+void GPIO_Set_AF(GPIO_TypeDef *port, uint8_t pin);
+uint8_t GPIO_Read(GPIO_TypeDef *port, uint8_t pin);
 
 #endif
