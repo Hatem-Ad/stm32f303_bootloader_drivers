00bd12ff262971f4ac5837323880878db486eae0
Hatem-Ad <adouani_hatem@yahoo.com>
2025-08-14 02:13:13 +0100

    update gpio.h

--- Drivers/GPIO/GPIO.h
+++ Drivers/GPIO/GPIO.h
@@ -1,16 +1,31 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-
-
 //GPIO Modes
-#define GPIO_MODE_INPUT     0x0
-#define GPIO_MODE_OUTPUT    0x1
-#define GPIO_MODE_AF        0x2
-#define GPIO_MODE_ANALOG    0x3
+typedef enum{
+    GPIO_MODE_INPUT  = 0x0,
+    GPIO_MODE_OUTPUT = 0x1,
+    GPIO_MODE_AF     = 0x2,
+    GPIO_MODE_ANALOG = 0x3
+} GPIO_mode_t;
 
-#define GPIO_OTYPE_PP       0x0 // Push-Pull
-#define GPIO_OTYPE_OD       0x1 // Open-Drain
+
+typedef enum {
+    GPIO_OTYPE_PP = 0x0, // Push-Pull
+    GPIO_OTYPE_OD = 0x1  // Open-Drain
+} GPIO_otype_t;
+
+typedef enum {
+    GPIO_SPEED_LOW    = 0,
+    GPIO_SPEED_MEDIUM = 1,
+    GPIO_SPEED_HIGH   = 2
+} GPPIO_speed_t;
+
+typedef enum {
+    GPIO_NPULL    = 0,
+    GPIO_PULLUP   = 1,
+    GPIO_PULLDOWN = 2
+} GPIO_pull_t;
 
 
 void GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode);
