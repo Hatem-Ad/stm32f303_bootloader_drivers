b69733085c387a2027703f2608d26ac104b03359
ad <adouani_hatem@yahoo.com>
2025-10-05 23:06:46 +0100

    Update bootloader enhancement

--- Bootloader/Bootloader.c
+++ Bootloader/Bootloader.c
@@ -1,29 +1,32 @@
 #include <Core_CortexM4.h>  // For Main Stack Pointer 
 #include "Bootloader.h"
-#include "GPIO.h"  // Use GPIO pin to trigger bootloader
-#include "UART.h"  // Receive firmware over UART
-#include "FLASH.h" // For Flash memory
+#include "GPIO.h"           // Use GPIO pin to trigger bootloader
+#include "UART.h"           // Receive firmware over UART
+#include "FLASH.h"          // For Flash memory
 #include "STM32F3xx.h"
-#include "SYSTICK.h"
+#include "SYSTICK.h"        // SysTick for delay/timeouts
 
 void Bootloader_Init(void) {
     // Initialize peripherals needed for bootloader
-    GPIO_Init(GPIOA, 0);
-    GPIO_Config(GPIOA, 0, GPIO_MODE_INPUT, GPIO_OTYPE_PP, GPIO_SPEED_LOW, GPIO_PULLUP);
+    GPIO_Init(C_BL_TRIGGER_PORT, C_BL_TRIGGER_PIN);
+    GPIO_Config(C_BL_TRIGGER_PORT, C_BL_TRIGGER_PIN, 
+                GPIO_MODE_INPUT, GPIO_OTYPE_PP, GPIO_SPEED_LOW, GPIO_PULLUP);
 
     UART_Init();
+    SYSTICK_Init(1000);     // 1 ms tick for delays and timeouts
 }
 
 uint8_t Bootloader_CheckForUpdate(void) {
     // Check if a specific pin is low to enter update mode
     if (GPIO_Read(C_BL_TRIGGER_PORT, C_BL_TRIGGER_PIN) == 0) {
-        return 1;  // Enter bootloader
+        return 1U;  // Enter bootloader
     }
     
-    return 0; //Jump to app
+    return 0U; //Jump to app
 }
 
-BootStatus_t Bootloader_ReceiveFirmware(void) {
+BootStatus_t Bootloader_ReceiveFirmware(void) 
+{
     // Local declaration
     uint8_t buffer[C_FW_CHUNK_SIZE];
     uint32_t addr = C_FLASH_APP_BASE;
@@ -32,7 +35,7 @@ BootStatus_t Bootloader_ReceiveFirmware(void) {
 
     UART_SendString("BL: erase app area...\r\n");
 
-    //1. Erase the application aera before writing 
+    // 1. Unlock Flash 
     status = FLASH_Unlock();
     if(status != FLASH_OK)
     {
@@ -42,7 +45,7 @@ BootStatus_t Bootloader_ReceiveFirmware(void) {
 
     FLASH_ClearFlags();
     
-    // Erase all application pages
+    // 2. Erase application area
     status = FLASH_EraseAppArea();
     if(status != FLASH_OK)
     {
@@ -53,7 +56,7 @@ BootStatus_t Bootloader_ReceiveFirmware(void) {
 
     UART_SendString("BL: receiving...\r\n");
 
-    //2. Received data chunks and write to flash
+    // 3. Received data chunks
     while (1)
     {
         received = UART_Receive((char *)buffer, (uint32_t)C_FW_CHUNK_SIZE, C_BL_UART_TIMEOUT); // Blocking read
@@ -68,7 +71,7 @@ BootStatus_t Bootloader_ReceiveFirmware(void) {
             received++;
         }
 
-        // Write to flash
+        // 4. Write to flash
         status = FLASH_Write(addr, buffer, received);
         if (status != FLASH_OK)
         {
@@ -90,7 +93,7 @@ BootStatus_t Bootloader_ReceiveFirmware(void) {
 
 void Bootloader_JumpToApp(void) {
 
-    uint32_t msp0 = *(volatile uint32_t *)C_FLASH_APP_BASE; // APP MSP
+    uint32_t msp0 = *(volatile uint32_t *)C_FLASH_APP_BASE;         // APP MSP
     uint32_t reset = *(volatile uint32_t *)(C_FLASH_APP_BASE + 4U); // APP ResetHandler
     // Function pointer to application's reset handler
     void (*App_reset_handler)(void);
@@ -110,15 +113,15 @@ void Bootloader_JumpToApp(void) {
 
     UART_SendString("BL: jumping to app\r\n");
 
-    // Disable interrupts if needed
+    // Disable interrupts
     __disable_irq();
 
-    //SysTick->CTRL = 0;
+    SYSTICK->CTRL = 0; // Disable SysTick before jumping
 
     // Relocate vector table - give the offset
-    SCB->VTOR = C_FLASH_APP_BASE & 0x1FFFFF80UL;
+    SCB->VTOR = C_FLASH_APP_BASE & 0xFFFFFF00UL;
     
-    // Set MSP from app's vector table to load app stack
+    // Set MSP from app's vector table to load new stack pointer
     __set_MSP(msp0);
 
     // For synchronization barriers
@@ -141,19 +144,24 @@ void Bootloader_run() {
         // Reciever a new firmware UART
         if (Bootloader_ReceiveFirmware() == E_BL_OK)
         {
-            UART_SendString("BL: update successfil\r\n");
+            UART_SendString("BL: update successful\r\n");
             Bootloader_JumpToApp(); //Jump to user application in flash
         }
-        else{
+        else
+        {
             UART_SendString("BL: update failed, staying in bootloader\r\n");
             while (1)
             {
-                // indicationa needed here to say where stay here in BL
+                GPIO_Toggle(GPIOE, 9);
+                SYSTICK_Delay(300);
             }
         } 
-    } else {
-            Bootloader_JumpToApp();
-           }
+    } 
+    else 
+    {
+        UART_SendString("BL: jumping to app\r\n");
+        Bootloader_JumpToApp();
+    }
 
 }
 
