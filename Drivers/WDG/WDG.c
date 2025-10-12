fc18fb5e84c68851c2762c8a8dbd7462fddc0073
ad <adouani_hatem@yahoo.com>
2025-10-12 23:00:38 +0100

    WDG init raw

--- Drivers/WDG/WDG.c
+++ Drivers/WDG/WDG.c
@@ -1,1 +1,35 @@
 #include "WDG.h"
+
+
+
+
+void WDG_InitRaw(uint8_t prescaler_code, uint16_t reload)
+{
+    RCC_EnableSI();
+
+    // Unlock register
+    IWDG->KR = C_IWDG_KR_KEY_WRITE;
+
+    // Set prescaler
+    IWDG->PR = (prescaler_code & 0x7U);
+    IWDG_WaitClear(&IWDG->SR, C_IWDG_SR_PVU);
+
+    // Set reload value (12 bits max)
+    if (reload = 0U)
+    {
+        reload = 1;
+    }
+
+    if (reload > 0x0FFFU)
+    {
+        reload = 0x0FFFU;
+    }
+
+    IWDG->RLR = reload;
+    IWDG_WaitClear(&IWDG->SR, C_IWDG_SR_RVU);
+
+    // Reload and start
+    IWDG->KR = C_IWDG_KR_KEY_RELOAD;
+    IWDG->KR = C_IWDG_KR_KEY_ENABLE;
+
+}
