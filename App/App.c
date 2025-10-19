7b4fbe79421ad10b9100bd29fb1b27796db92341
ad <adouani_hatem@yahoo.com>
2025-10-20 00:38:28 +0100

    update app.c file instructions

--- App/App.c
+++ App/App.c
@@ -1,17 +1,20 @@
 #include "STM32F3xx.h"
 #include "GPIO.h"
 #include "UART.h"
+#include "SysTick.h"
 
 int main(void)
 {
-    GPIO_Init();
+    SystemInit();
     UART_Init();
+    GPIO_InitPort(GPIOE);
+    GPIO_Config(GPIOE, 9, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO_SPEED_HIGH, GPIO_NPULL);
 
     UART_SendString("Hello from the Application !\r\n");
     while (1)
     {
         GPIO_TogglePin(GPIOE, 9);   // Blink LED
-        for (volatile uint32_t i = 0; i < 500000; i++);
+        SysTick_DelayMs(500);
         
     }
     
