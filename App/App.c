731b41fce259d9dd7ef21cd371ea3a38343d4dc0
ad <adouani_hatem@yahoo.com>
2025-11-03 23:25:21 +0100

    App led bug

--- App/App.c
+++ App/App.c
@@ -16,12 +16,18 @@ int main(void)
 
     // Initialize LED pin (PE9)
     GPIO_InitPort(GPIOE);
-    GPIO_Config(GPIOE, 9, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO_SPEED_HIGH, GPIO_NPULL);
+    GPIO_Config(GPIOE, 9, E_GPIO_MODE_OUTPUT, E_GPIO_OTYPE_PP, E_GPIO_SPEED_HIGH, E_GPIO_NPULL);
+    GPIO_Config(GPIOE, 8, E_GPIO_MODE_OUTPUT, E_GPIO_OTYPE_PP, E_GPIO_SPEED_HIGH, E_GPIO_NPULL);
+
 
     while (1)
     {
+        UART_SendString("App running OK\r\n");
         GPIO_Toggle(GPIOE, 9);   // Blink LED
         SysTick_DelayMs(500);
+        GPIO_InitPort(GPIOE);
+        GPIO_Toggle(GPIOE, 8);
+        for (volatile int i = 0; i < 1000000; i++);
     }
 
     return 0;
