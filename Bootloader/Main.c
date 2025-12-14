6e5b08282a7214d903a07454f5fe34c9eeea4e64
ad <adouani_hatem@yahoo.com>
2025-12-14 14:41:41 +0100

    Boot Debugging

--- Bootloader/Main.c
+++ Bootloader/Main.c
@@ -1,6 +1,11 @@
 #include "Bootloader.h"
 
+__attribute__((section(".debugvars")))
+volatile uint32_t maindbg = 0;
+
 int main(void) {
+
+    maindbg = 0x77777777;   // DEBUG
 //Execute main program logic
     Bootloader_run();
 // loop while current is on
